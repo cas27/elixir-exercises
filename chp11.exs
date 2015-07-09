f86392b032668b08e4ab613663633e6db543346a
Cory Schmitt <cory@schmitty.me>
2015-07-09 16:44:55 -0700

    Parse char strings

--- chp11.exs
+++ chp11.exs
@@ -6,4 +6,24 @@ defmodule Chp11 do
   def anagram?(word1, word2) do
     word1 == Enum.reverse(word2)
   end
+
+  def calculate(problem) when is_list(problem) do
+    problem
+    |> split_on_sign
+    |> calculate
+  end
+  def calculate({x, [?+ | t]}), do: List.to_integer(x) + List.to_integer(t)
+  def calculate({x, [?- | t]}), do: List.to_integer(x) - List.to_integer(t)
+  def calculate({x, [?* | t]}), do: List.to_integer(x) * List.to_integer(t)
+  def calculate({x, [?/ | t]}), do: List.to_integer(x) / List.to_integer(t)
+
+  def split_on_sign(problem) do
+    cond do
+      Enum.any? problem, &([&1] == '+') -> Enum.split_while problem, &([&1] != '+')
+      Enum.any? problem, &([&1] == '-') -> Enum.split_while problem, &([&1] != '-')
+      Enum.any? problem, &([&1] == '*') -> Enum.split_while problem, &([&1] != '*')
+      Enum.any? problem, &([&1] == '/') -> Enum.split_while problem, &([&1] != '/')
+      :else -> raise "Malformed Problem"
+    end
+  end
 end
