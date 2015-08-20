5b9d1324c8e124a819e02d4d685b60958c7ba4d0
Cory Schmitt <cory@schmitty.me>
2015-08-20 11:37:35 -0700

    Client API

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -1,6 +1,18 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: :stack)
+  end
+
+  def pop do
+    GenServer.call(:stack ,:pop)
+  end
+
+  def push(el) do
+    GenServer.cast(:stack, {:push, el})
+  end
+
   def handle_call(:pop, _from, [h|t]) do
     {:reply, h, t}
   end
